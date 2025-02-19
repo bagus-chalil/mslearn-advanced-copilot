b8d942cc27635f6c50b3632c4e52bd60dd21e339
Mohammad Bagus Chalil Akbar <62526478+bagus-chalil@users.noreply.github.com>
2025-02-19 13:29:12 +0000

    Add a new route

--- main.py
+++ main.py
@@ -31,6 +31,14 @@ def countries():
     return list(data.keys())
 
 
+@app.get('/countries/{country}/cities')
+def cities(country: str):
+    """
+    Returns a list of cities for a given country/region.
+    """
+    return list(data[country].keys())
+
+
 @app.get('/countries/{country}/{city}/{month}')
 def monthly_average(country: str, city: str, month: str):
     return data[country][city][month]
